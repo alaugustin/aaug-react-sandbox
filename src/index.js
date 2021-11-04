a159c6a7f4aa4d7f6b239d3f8ed9b662097527aa
aaug <aaug@caasco.ca>
2021-11-03 20:58:43 -0400

    wiring in hello world component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Helloworld from './components/HelloWorld';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Helloworld />
   </React.StrictMode>,
   document.getElementById('root')
 );
