061e72c2d36e440919c7a5ac4e2e7f887a27fd48
aaug <aaug@caasco.ca>
2021-12-09 11:24:24 -0500

    minor edit

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -4,7 +4,7 @@ function Hooks() {
     const [count, setCount] = useState(0);
     return (
         <div>
-            <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">State Hook</h1>
+            <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">Hooks</h1>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                 Click me
