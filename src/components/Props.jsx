11d1ac781ac037a5ae9e09e63023cdb9c2b5a981
aaug <aaug@caasco.ca>
2021-12-09 15:39:26 -0500

    extrcting props object

--- src/components/Props.jsx
+++ src/components/Props.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
-function Props(props) {
+function Props({name}) {
     // props.name is data from the user defined component props from the component: <Props name = "Encarnacion" />
     return (
-        <div className="border-b-2 pt-4 pb-2">
-            <h1 className="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">Hello Props</h1>
-            <p>Welcome {props.name}</p>
+        <div className="container mx-auto px-4 border-b-2 pt-4 pb-2">
+            <h2 className="text-2xl font-normal leading-normal mt-0 mb-2 text-black-800">What are Props?</h2>
+
+            <ul className="pb-4">
+                <li>a single object argument with data which returns a react element</li>
+            </ul>
+
+
+            <p className="font-mono pb-4">The name in the exapmle below is returened from the component props</p>
+            <p className="font-mono">Welcome <span className="bg-black text-white p-1">{name}</span></p>
         </div>
     );
 }
