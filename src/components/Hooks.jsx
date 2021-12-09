72c52616427eb5d905f1f45aecee3813aee4990a
aaug <aaug@caasco.ca>
2021-12-09 11:20:34 -0500

    declaring multiple variables

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 
 function Hooks() {
     const [count, setCount] = useState(0);
+    const [age, setAge] = useState(42);
+    const [fruit, setFruit] = useState('tamarind');
+    const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+
+    console.log([{ count: useState(count) }, { age: useState(age) }, { fruit: useState(fruit) }, { todos: useState(todos) }])
+
     return (
         <div>
-            <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">State Hook</h1>
+            <h1 className="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">State Hook</h1>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            <button onClick={() => setCount(count + 1)} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                 Click me
             </button>
         </div>
