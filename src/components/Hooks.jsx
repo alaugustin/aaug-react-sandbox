e2c7c1dd2f1377f23332e0b3b99117c72b6cbc6c
aaug <aaug@caasco.ca>
2021-12-09 11:03:05 -0500

    example

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -1,17 +1,16 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
 function Hooks() {
     const [count, setCount] = useState(0);
-
-
-
-
-
-
-
-
-
-    return <h1 className={"font-sans text-3xl font-bold"}>Hello hooks!</h1>;
+    return (
+        <div>
+            <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-black-800">State Hook</h1>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                Click me
+            </button>
+        </div>
+    );
 }
 
 export default Hooks;
