d56627440d3799dfb2f4d1fdfd96cab0011852f6
aaug <aaug@caasco.ca>
2021-12-10 13:08:58 -0500

    fetching data with asynchronous function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,23 @@ import React, { useEffect } from 'react';
 import "../index.css";
 
 function App() {
+
   useEffect(() => {
-    const url = "https://reqres.in/api/users"
+    const url = "https://reqres.in/api/users";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+
+    fetchData();
   }, []);
   return <div></div>;
-}
+};
 
 export default App;
