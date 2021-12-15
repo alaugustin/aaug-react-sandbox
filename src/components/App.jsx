44a42e3b7dfd34b0bfc340bf553bbc21bd716058
aaug <aaug@caasco.ca>
2021-12-15 15:19:09 -0500

    app displays contact on screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import "../index.css";
 
 function App() {
   const [support, setSupport] = useState("");
+  const [teamList, setTeamList] = useState([]);
 
   useEffect(() => {
     const url = "https://reqres.in/api/users";
@@ -11,9 +12,9 @@ function App() {
       try {
         const response = await fetch(url);
         const json = await response.json();
-        setSupport(json.support.text);
 
-        console.log(json.data);
+        setSupport(json.support.text);
+        setTeamList(json.data);
       } catch (error) {
         console.log("error", error);
       }
@@ -21,7 +22,33 @@ function App() {
 
     fetchData();
   }, []);
-  return <div><p>{support}</p></div>;
+
+  return (
+    <section className='p-9 dark:bg-black'>
+      <h1 className='text-5xl mb-8'>Contact our team</h1>
+
+      <div className='flex justify-between mb-16'>
+        {
+          teamList.map(item =>
+            <div key={item.id} className='border-solid border-2 border-grey-600 py-4 px-8 rounded-md'>
+              <img src={item.avatar} alt={`${item.first_name} ${item.last_name}`} className='rounded-full flex items-center justify-center mb-2'/>
+              <p>
+                {item.first_name} {item.last_name}
+              </p>
+
+              <a href={`mailto:${item.email}`} className='text-blue-600 hover:text-blue-800 hover:underline'>Email {item.first_name} {item.last_name}</a>
+            </div>
+          )
+        }
+      </div>
+
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-8'>
+        Toggle dark mode
+      </button>
+
+      <p>{support}</p>
+    </section>
+  );
 };
 
 export default App;
